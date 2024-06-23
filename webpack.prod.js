2c062ac4362267241d2338a137013fa2a15f4cd6
kos4 <kos4.kks@gmail.com>
2024-06-23 21:12:15 +0500

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const common = require("webpack.common");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
